c522aa4947ea359b3905c5e2247632bfb03fe5cf
sushmishra87 <sm.sushmimishra@gmail.com>
2023-02-25 03:48:25 +0000

    Completed Lab ReturnArrayContentsTest

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Lab {
@@ -29,7 +30,13 @@ public class Lab {
      * @param nums the array to be manipulated.
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
-    public String arrayToString(int[] nums){
-        return "";
+    public String arrayToString(int[] nums)
+    {
+    	String str = new String();
+    	for(int i = 0; i < nums.length; i++)
+    	{
+    		str += nums[i] + " ";
+    	}
+    	return str;
     }
 }
